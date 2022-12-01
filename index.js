c34171e337744d8dcb43a8aaa405b9fcfe8d13a9
Bode He <bode_he@epam.com>
2022-12-01 15:03:03 +0800

    feat: add files

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const CONFIG = require('./config/config')
 
 // 帐号密码
 // docker 分配的 ip 地址
-mongoose.connect("mongodb://bode:bode@mongo:27017/?authSource=admin")
+const url = `mongodb://${CONFIG.MONG_USER}:${CONFIG.MONGO_PASSWORD}@${CONFIG.MONGO_IP}:${CONFIG.MONGO_PORT}/?authSource=admin`
+mongoose.connect(url)
     .then(() => console.log('successfully connected to DB'))
     .catch((e) => console.log('e', e));
 
